6b347d0266140111d1a451f19cd10b07ab3abcea
Alamito <alamirbobroski12@gmail.com>
2023-10-28 19:55:11 -0300

    finished home

--- components/home.js
+++ components/home.js
@@ -1,9 +1,17 @@
+import styles from '../src/styles/home.module.css'
+
 const Home = () => {
     return (
-        <div>
-            <h1>
-                Home
-            </h1>
+        <div className={styles['container-home']} id='home'>
+            <img src="/img/neko-anotando.png" alt="neko-anotando" className={styles['home-img']}/>
+            <div>
+                <h1 className={styles['home-text']}>
+                    <div className={styles['text-poke']}>POKE</div>
+                    <span className={styles['text-ide']}>IDE</span>
+                    <span className={styles['text-poke']}>X</span>
+                </h1>
+                <button className={styles['home-button']}>START</button>
+            </div>
         </div>
     )
 }
