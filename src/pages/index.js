88db3fb05f607332f3470c2335eb41103d3eaa9e
Kelvin Brose <ksb_56@hotmail.com>
2023-10-31 17:05:38 -0300

    create inicio (incompleto)
    Apenas coloquei todos elementos de d pagina "inicio" falta organiza-los fazer as caixas e arrumar o codigo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
-export default function Home() {
-  return (
-    <h1>
-      Olá, mundo!
-    </h1>
-  )
-}
+
+  import Inicio from "../../components/inicio"
+  
+  export default function Page() {
+    return (
+      <div>
+        <Inicio />
+        <h1>
+          Olá, mundo!!!
+        </h1>
+      </div>
+    )
+    }
