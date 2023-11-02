b0af9b780dd24db5b51083f20143899c4c602a1f
llmeregalli <lukinhasmeregalli@gmail.com>
2023-11-01 23:10:44 -0300

    Parte da Home Section

--- src/components/sections/home.tsx
+++ src/components/sections/home.tsx
@@ -1,7 +1,25 @@
+import Styles from "../../styles/home.module.css"
+import Borders from "../../styles/borders.module.css"
+
+
 export default function HomeSection() {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen w-full bg-idePurple snap-start">
-      Home section
+    
+    <section className={Styles.section}>
+        <img
+          className={Styles.img}
+          src="/img/neko-anotando.png"
+        />
+  
+        <div className={Styles.titulo}>
+          <p>POKE</p>
+          <p>
+            <span className={Styles.textoVermelho}>IDE</span>
+          X</p>
+          <div className={Borders.pixelCorners + " " + Borders.white + " " + Styles.botao}>
+              <a href="#" >Start</a>
+          </div>
+        </div>
     </section>
   )
 }
